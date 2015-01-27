b6b8a800251d53d73b6683f605f14cec204177d7
Hannah Carney <hannahcarneyart@gmail.com>
2015-01-27 00:53:48 +0000

    rewritten entirely

--- js/bowling.js
+++ js/bowling.js
@@ -1,144 +1,49 @@
-var Bowling = function() {
-  this.player = "Player1";
-  this.pins = 10;
-  this.attempt = 1;
-  this.strikes = [];
-  this.spares = [];
-  this.frameNo = 1;
-  this.totalPoints = 0;
-  this.bonusPointsStrike = 0;
-  this.bonusPointsSpare = 0;
-  this.pins1 = 0;
-  this.pins2 = 0
+// 
+var Game = function () {
+  this._rolls = [];
+  this._current = 0;
 };
 
-Bowling.prototype.newTurn = function() {
-  if (this.attempt === 1)
-  {
-    this.attempt += 1
+Game.prototype.roll = function (pins) {
+  if (typeof pins !== 'number') {
+      throw new Error('expected a number');
   }
-  else if (this.attempt === 2)
-  {	
-    this.attempt -= 1
-    this.totalScore()
-    this.maxFrames()
-	  this.pins = 10
-  }
+  this._rolls[this._current++] = pins;
 };
 
-Bowling.prototype.pinsDown = function(number) {
-  if (number === 10)
-  {
-    this.strikes.push(10 * (this.strikes.length + 1))
-	  this.attempt += 1
-	  this.newTurn()
+Game.prototype.score = function () {
+  var score = 0, i,
+    hasBonusRoll = this._hasBonusRoll();
+    scoringRolls = (hasBonusRoll) ? hasBonusRoll + 1 : this._rolls.length;
+
+  for (i = 0; i < scoringRolls; i++) {
+    if (this._isStrike(i)) {
+      score += 10 + this._rolls[i + 1] + this._rolls[i + 2];
+    }
+    else if (this._isSpare(i)) {
+      score += 10 + this._rolls[i + 2];
+      i ++;
+    }
+    else 
+    {
+      score += this._rolls[i];
+    }
   }
-  else
-  {
-    this.pins = (this.pins - number)
-	  this.isASpare()
-    this.newTurn()
-  }
+    return score;
 };
 
-Bowling.prototype.maxFrames = function() {
-  if (this.frameNo === 10)
-  {
-    this.frameNo = 1
-	return "This is the end of the game"
-  }
-  else 
-  {
-	this.frameNo += 1
-  }
+Game.prototype._isSpare = function (roll) {
+  return (this._rolls[roll] + this._rolls[roll + 1] === 10);
 };
 
-Bowling.prototype.isASpare = function() {
+Game.prototype._isStrike = function (roll) {
+  return this._rolls[roll] === 10;
+}
 
-  if (this.pins === 0 && this.spares.length === 0)
-  {
-    this.spares.push(10 * (this.spares.length + 1))
+Game.prototype._hasBonusRoll = function () {
+  var tenthFrame = this._rolls.length - 3, hasBonus;
 
-  }
-  else if (this.pins === 0 && this.spares.length >= 1)
-  {
-	this.spares.push(this.pins1)
-	this.spares.push(10)
-  }
-  else if (this.attempt === 1)
-  {
-    this.pins1 = (10 - this.pins)
-  }
-  else if (this.attempt === 2 && this.strikes.length === 1)
-  {
-    this.pins2 = ((10 - this.pins) - this.pins1)
-    this.strikes.push(this.pins1 + this.pins2)
-    this.strikes.push(this.pins1 + this.pins2)
-    this.resetSpareCount()
-    this.resetStrikeCount()
-  }
-  else if (this.attempt === 2 && this.strikes.length === 2)
-  {
-    this.pins2 = ((10 - this.pins) - this.pins1)
-    this.strikes.push(this.pins1)
-    this.strikes.push(this.pins1 + this.pins2)
-    this.strikes.push(this.pins1 + this.pins2)
-    this.resetSpareCount()
-	this.resetStrikeCount()
+  hasBonus = (this._isStrike(tenthFrame) || this._isSpare(tenthFrame));
 
-  }
-  else if (this.attempt === 2 && this.pins != 0)
-  {
-	  this.pins2 = ((10 - this.pins) - this.pins1)
-    this.spares.push(this.pins1)
-	  this.resetSpareCount()
-	  this.resetStrikeCount()
-  }
-};
-
-Bowling.prototype.totalScore = function() {
-  if (this.strikes.length != 0)
-  {
-    this.addStrikes()
-  }
-
-  else if (this.spares.length != 0)
-  {
-	  this.addSpares()
-  }
-
-  else if (this.bonusPointsSpare > 0)
-  {
-	  (this.totalPoints = (10 - this.pins + (this.bonusPointsSpare)) + (this.pins1) + this.totalPoints)
-	  this.bonusPointsSpare = 0
-  }
-  else if (this.bonusPointsStrike > 0)
-  {
-	  (this.totalPoints = (10 - this.pins + (this.bonusPointsStrike)) + (this.pins1 + this.pins2) + this.totalPoints)
-    this.bonusPoints = 0
-  }
-
-  else
-  {
-	  this.totalPoints = this.totalPoints + this.pins1 + this.pins2
-  }
-};
-
-Bowling.prototype.resetStrikeCount = function() {
-  
-  if (this.pins != 10)
-    this.strikes = []
-};
-
-Bowling.prototype.resetSpareCount = function() {
- if (this.pins != 10)
-   this.spares = []	
-};
-
-Bowling.prototype.addStrikes = function() {
-  this.bonusPointsStrike = (eval(this.strikes.join('+')));
-};
-
-Bowling.prototype.addSpares = function() {
-  this.bonusPointsSpare = (eval(this.spares.join('+')));
+  return (hasBonus) ? tenthFrame : null;
 };
--- spec/bowling_spec.js
+++ spec/bowling_spec.js
@@ -1,104 +1,95 @@
-describe("Bowling", function(){
+describe("Bowling", function() {
 
-  var bowling;
+  var rollMany, rollSpare, rollStrike;
 
-  beforeEach(function() {
-    bowling = new Bowling();
+  rollMany = function (rolls, pins) {
+    var i;
+    for (i = 0; i < rolls; i++) {
+      this.roll(pins);
+    }
+  };
+
+  rollSpare = function () {
+    this.roll(5);
+    this.roll(5);
+  };
+
+  rollStrike = function () {
+    this.roll(10);
+  };
+
+  beforeEach(function () {
+    this.game = new Game();
   });
 
-  describe("by default", function(){
-
-    it("starts with one player", function(){
-      expect(bowling.player).toBe("Player1");
-    });
-
-    it("starts with 10 pins", function(){
-      expect(bowling.pins).toEqual(10);
-    });
-
-    it("starts on frame 1", function(){
-      expect(bowling.frameNo).toEqual(1)
-    });
-  });
-
-  describe("on players turn", function(){
-
-    it("can hit pins", function() {
-      bowling.pinsDown(9)
-      expect(bowling.pins).toEqual(1)
-    });
-
-    it("can have strikes", function(){
-      bowling.pinsDown(10)
-      expect(bowling.bonusPointsStrike).toEqual(10)
-    });
-
-    it("can have spares", function(){
-      bowling.pinsDown(7)
-      bowling.pinsDown(3)
-      expect(bowling.bonusPointsSpare).toEqual(10)
-    });
-
-    it("player can have only two attempts per turn", function(){
-      expect(bowling.attempt).toBe(1)
-      bowling.pinsDown(6)
-      expect(bowling.attempt).toBe(2)
-    });
-
-    it("goes up a frame after second attempt", function(){
-      bowling.pinsDown(10)
-      expect(bowling.frameNo).toEqual(2)
-    });
-
-  });
-
-  describe("scoring system", function(){
-
-    it("a strike waits for the next score to be tallied", function(){
-      bowling.pinsDown(10)
-      expect(bowling.totalPoints).toEqual(0)
-    });
-
-    it("a spare waits for the next score to be tallied", function(){
-      bowling.pinsDown(5)
-      bowling.pinsDown(5)
-      expect(bowling.totalPoints).toEqual(0)
-    });
-
-    it("after a strike the next two rolls are added", function(){
-      bowling.pinsDown(10)
-      bowling.pinsDown(3)
-      bowling.pinsDown(2)
-      expect(bowling.totalPoints).toEqual(20)
-    });
-
-    it("after a spare the next one roll is added", function(){
-      bowling.pinsDown(5)
-      bowling.pinsDown(5)
-      bowling.pinsDown(5)
-      bowling.pinsDown(1)
-      expect(bowling.totalPoints).toEqual(21)
-    });
-
-    it("after two strikes in a row", function() {
-      bowling.pinsDown(10)
-      bowling.pinsDown(10)
-      bowling.pinsDown(1)
-      bowling.pinsDown(1)
-      expect(bowling.totalPoints).toEqual(35)
-    });
-
-    it("after two spares in a row", function() {
-      bowling.pinsDown(5)
-      bowling.pinsDown(5)
-      bowling.pinsDown(5)
-      bowling.pinsDown(5)
-      bowling.pinsDown(5)
-      bowling.pinsDown(5)
-      bowling.pinsDown(2)
-      bowling.pinsDown(2)
-      expect(bowling.totalPoints).toEqual(46)
+  describe('Gutter Game', function() {
+    
+    it("should score 0 given all rolls hit no pins", function () {
+      rollMany.call(this.game, 20, 0);
+      expect(this.game.score()).toEqual(0);
     });
   });
 
+  describe("Game when all rolls hit only 1 pin", function () {
+    
+    it("should score 20 given all rolls hit 1 pin", function() {
+      rollMany.call(this.game, 20, 1);
+      expect(this.game.score()).toEqual(20);
+    });
+  });
+
+  describe("Game with one spare", function () {
+
+    it("should score 20 given the 1st 3 rolls hit 5 pins", function () {
+      rollSpare.call(this.game);
+      this.game.roll(5);
+      rollMany.call(this.game, 17, 0)
+      expect(this.game.score()).toEqual(20);
+    });
+  });
+
+  describe("Game with one strike", function () {
+
+    it("should score 26 followed by 2 rolls hitting 5 & 3", function (){
+      rollStrike.call(this.game);
+      this.game.roll(5);
+      this.game.roll(3);
+      rollMany.call(this.game, 17, 0)
+      expect(this.game.score()).toEqual(26);
+    });
+  });
+
+  describe('Perfect Game', function () {
+
+    it("should score 300 given 12 rolls hit 10 pins each", function () {
+      rollMany.call(this.game, 12, 10);
+      expect(this.game.score()).toEqual(300);
+    });
+  });
+
+  describe("A full game", function () {
+
+    it("should score 110", function () {
+      this.game.roll(7);
+      this.game.roll(2);
+      this.game.roll(6);
+      this.game.roll(1);
+      rollSpare.call(this.game);
+      this.game.roll(3);
+      this.game.roll(4);
+      rollSpare.call(this.game);
+      rollStrike.call(this.game);
+      this.game.roll(5);
+      this.game.roll(3);
+      this.game.roll(5);
+      this.game.roll(0);
+      this.game.roll(6);
+      this.game.roll(2);
+      this.game.roll(7);
+      this.game.roll(3);
+      this.game.roll(5);
+      expect(this.game.score()).toEqual(110);
+
+    });
+  });
 });
